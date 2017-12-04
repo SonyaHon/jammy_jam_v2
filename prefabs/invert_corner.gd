d6d07131c56337d6750170992fa2ab75d51d88ef
Dmitry Borovkov <CaptainJustice@MacBook-Air-Dmitry.local>
2017-12-04 21:04:18 +0300

    Level and new sprites

---
+++ prefabs/invert_corner.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	add_to_group(Utils.GR_WALL);
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
